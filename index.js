f7f0940f55b4164f724bc5c713245656bf611017
EldinKecap <76916396+EldinKecap@users.noreply.github.com>
2022-10-22 00:13:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, nam nemo, amet eaque expedita dignissimos nulla voluptate excepturi ex fugit totam fugiat. Quos itaque, vitae officiis nostrum veritatis dignissimos vero!';
+
+function startTyping(){
+    let typingBox = document.getElementById('typingBox');
+    // typingBox.innerHTML = '';
+    // scrambler
+    // for (let i = 0; i < text.length; i++) {
+    //     let timeoutTime = Math.floor(Math.random()*10000);
+    //     setTimeout(()=>{
+    //         typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
+    //         i++;
+    //         console.log(timeoutTime);
+    //     },timeoutTime);
+    // }
+    //proper Output
+    // let intervalTime = Math.random()*1000;
+    // let i = 0;
+    // let typingInterval = setInterval(()=>{
+    //     intervalTime = Math.random()*10000;
+    //     typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
+    //     i++;
+    //     if (i >= text.length) {
+    //         clearInterval(typingInterval);
+    //     }
+    //     console.log(intervalTime);
+    // },intervalTime)
+    let i = 0;
+    // (function loop() {
+    //     var rand = Math.round(Math.random() * 500);
+    //     setTimeout(function() {
+    //         typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
+    //         i++;
+    //         if (i >= text.length) {
+    //             return;
+    //         }
+    //             loop();  
+    //     }, rand);
+    // }());
+
+    function smth() {
+        var rand = Math.round(Math.random() * 500);
+        setTimeout(function() {
+            typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
+            i++;
+            if (i >= text.length) {
+                return;
+            }
+    }
+
+}
+
