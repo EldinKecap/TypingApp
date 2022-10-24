a6b59ea3c9fe5c7a31cdd9b09ccb5cc7e36de1dc
Eldin Kecap <eldinkecap@gmail.com>
2022-10-24 12:51:09 +0200

    added a fastTypingFunction

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ async function startTyping(){
     let newElement = document.createElement('p');
     typingBox.appendChild(newElement)
     function loop() {
-        var rand = Math.round(Math.random() * 300);
+        var rand = Math.round(Math.random() * 250);
         setTimeout(function() {
-            newElement.innerHTML = newElement.innerHTML.concat(text[i]);
+            newElement.innerHTML = text.slice(0,i);
             i++;
-        
+            if (rand > 240) {
+                let letters = ['a','e','i','o','u'];
+                newElement.innerHTML = newElement.innerHTML.concat(letters[Math.floor(Math.random()*4)]);
+                
+                i-=2;             
+            }
+            
+            
             if (i >= text.length) {
                 return;
             }
@@ -29,8 +36,47 @@ async function startTyping(){
 
 }
 
+async function fastTyping(){
+    let typingBox = document.getElementById('typingBox');
+    let text = await fetchData();
+    text = text.joke
+    let i = 0;
+    let newElement = document.createElement('p');
+    typingBox.appendChild(newElement)
+    function loop() {
+        var rand = Math.round(Math.random() * 100);
+        setTimeout(function() {
+            newElement.innerHTML = text.slice(0,i);
+            i++;
+            if (rand > 95) {
+                let letters = ['a','e','i','o','u'];
+                newElement.innerHTML = newElement.innerHTML.concat(letters[Math.floor(Math.random()*4)]);  
+                i-=2;             
+            }
+            
+            
+            if (i >= text.length) {
+                return;
+            }
+                loop();  
+        }, rand);
+    };
+
+    loop();
+
+}
+
+
+
+
 let typeOutBtn = document.getElementById('typeOut');
-console.log(typeOutBtn);
+// console.log(typeOutBtn);
 typeOutBtn.addEventListener('click',()=>{
     startTyping()
+});
+
+let typeOutFastBtn = document.getElementById('typeOutFast');
+// console.log(typeOutBtn);
+typeOutFastBtn.addEventListener('click',()=>{
+    fastTyping()
 });
