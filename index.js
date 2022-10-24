093fc48782f3380c9408bacd4b1f3d5c76e9d8b9
Eldin Kecap <eldinkecap@gmail.com>
2022-10-24 11:39:14 +0200

    Finished first version of the tell me a joke app

--- index.js
+++ index.js
@@ -1,76 +1,36 @@
-let text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, nam nemo, amet eaque expedita dignissimos nulla voluptate excepturi ex fugit totam fugiat. Quos itaque, vitae officiis nostrum veritatis dignissimos vero!';
+async function fetchData() {
+    let response = await fetch('https://v2.jokeapi.dev/joke/Any?type=single');
+    let data = await response.json();
+    console.log(data);
+    return data;
+}
 
-function startTyping(){
+async function startTyping(){
     let typingBox = document.getElementById('typingBox');
-    let arrOfSetTimeouts = [];
-
-    if (typingBox.innerHTML != '') {
-        arrOfSetTimeouts
-    }
-
-    typingBox.innerHTML = '';
-
-    // scrambler
-    // for (let i = 0; i < text.length; i++) {
-    //     let timeoutTime = Math.floor(Math.random()*10000);
-    //     setTimeout(()=>{
-    //         typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
-    //         i++;
-    //         console.log(timeoutTime);
-    //     },timeoutTime);
-    // }
-    //proper Output
-    // let intervalTime = Math.random()*1000;
-    // let i = 0;
-    // let typingInterval = setInterval(()=>{
-    //     intervalTime = Math.random()*10000;
-    //     typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
-    //     i++;
-    //     if (i >= text.length) {
-    //         clearInterval(typingInterval);
-    //     }
-    //     console.log(intervalTime);
-    // },intervalTime)
+    let text = await fetchData();
+    text = text.joke
     let i = 0;
-    (function loop() {
-        var rand = Math.round(Math.random() * 500);
+    let newElement = document.createElement('p');
+    typingBox.appendChild(newElement)
+    function loop() {
+        var rand = Math.round(Math.random() * 300);
         setTimeout(function() {
-            typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
+            newElement.innerHTML = newElement.innerHTML.concat(text[i]);
             i++;
+        
             if (i >= text.length) {
                 return;
             }
                 loop();  
         }, rand);
-    }());
-    //Radi i nije modifikovano
-    // (function loop() {
-    //     var rand = Math.round(Math.random() * 500);
-    //     setTimeout(function() {
-    //         typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
-    //         i++;
-    //         if (i >= text.length) {
-    //             return;
-    //         }
-    //             loop();  
-    //     }, rand);
-    // }());
+    };
 
-//     function smth() {
-//         var rand = Math.round(Math.random() * 10500);
-//         setTimeout(function() {
-//             typingBox.innerHTML = typingBox.innerHTML.concat(text[i]);
-//             i++;
-//             if (i >= text.length) {
-//                 return;
-//             }
-//     });
-
-// }
-//     for (let j = 0; j < text.length; j++) {
-//         smth();
-//         console.log('bog');
-//     }
-
+    loop();
 
 }
+
+let typeOutBtn = document.getElementById('typeOut');
+console.log(typeOutBtn);
+typeOutBtn.addEventListener('click',()=>{
+    startTyping()
+});
