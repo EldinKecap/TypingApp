ebdfeeef523ac1aa430919fc4e43bf4da9c4003a
Eldin Kecap <eldinkecap@gmail.com>
2022-10-25 12:44:20 +0200

    added a user input functionality and added a light mode

--- index.js
+++ index.js
@@ -5,22 +5,29 @@ async function fetchData() {
     return data;
 }
 
-async function startTyping(){
+async function startTyping(typingTime){
     let typingBox = document.getElementById('typingBox');
-    let text = await fetchData();
-    text = text.joke
+    let userInput = document.getElementById('userInput');
+    let text = userInput.value;
+    userInput.value = '';
+    console.log(text);
+    if (text == '') {
+        text = await fetchData();
+        text = text.joke
+    }
     let i = 0;
     let newElement = document.createElement('p');
     let blinker = document.createElement('span');
     blinker.innerHTML = '|';
     typingBox.appendChild(newElement);
+    console.log(typingTime-typingTime/100*5);
     function loop() {
-        var rand = Math.round(Math.random() * 250);
+        var rand = Math.round(Math.random() * typingTime);
         setTimeout(function() {
+            i++;
             newElement.innerHTML = text.slice(0,i);
             newElement.appendChild(blinker);
-            i++;
-            if (rand > 240) {
+            if (rand > typingTime-typingTime/100*5) {
                 let letters = ['a','e','i','o','u'];
                 newElement.removeChild(blinker);
                 newElement.innerHTML = newElement.innerHTML.concat(letters[Math.floor(Math.random()*4)]);
@@ -41,55 +48,34 @@ async function startTyping(){
     
 }
 
-async function fastTyping(){
-    let typingBox = document.getElementById('typingBox');
-    let text = await fetchData();
-    text = text.joke
-    let i = 0;
-    let newElement = document.createElement('p');
-    let blinker = document.createElement('span');
-    blinker.innerHTML = '|';
-    typingBox.appendChild(newElement);
-    function loop() {
-        var rand = Math.round(Math.random() * 100);
-        setTimeout(function() {
-            newElement.innerHTML = text.slice(0,i);
-            newElement.appendChild(blinker);
-            
-            i++;
-            if (rand > 95) {
-                let letters = ['a','e','i','o','u'];
-                newElement.removeChild(blinker);
-                newElement.innerHTML = newElement.innerHTML.concat(letters[Math.floor(Math.random()*4)]);  
-                newElement.appendChild(blinker)
-                i--;             
-            }
-            
-            
-            if (i > text.length) {
-                blinker.className = 'blink';
-                return;
-            }
-                loop();  
-        }, rand);
-        // console.log('ye');
-    };
-
-    loop();
-
-}
-
-
-
 
 let typeOutBtn = document.getElementById('typeOut');
 // console.log(typeOutBtn);
 typeOutBtn.addEventListener('click',()=>{
-    startTyping()
+    startTyping(250)
 });
 
 let typeOutFastBtn = document.getElementById('typeOutFast');
 // console.log(typeOutBtn);
 typeOutFastBtn.addEventListener('click',()=>{
-    fastTyping()
+    startTyping(150)
+});
+
+let typeOutSlowBtn = document.getElementById('typeOutSlow');
+// console.log(typeOutBtn);
+typeOutSlowBtn.addEventListener('click',()=>{
+    startTyping(500);
+});
+
+let modeSlider = document.getElementById('mode');
+modeSlider.addEventListener('click',()=>{
+    let modeButton = document.getElementById('modeButton');
+    let cssLink = document.getElementById('cssLink');
+    if (cssLink.getAttribute('href').includes('LM')) {
+        modeButton.style.animationName='slideDM';
+        cssLink.setAttribute('href','./styleDM.css');
+    }else{
+        modeButton.style.animationName='slideLM';
+        cssLink.setAttribute('href','./styleLM.css');
+    }
 });
